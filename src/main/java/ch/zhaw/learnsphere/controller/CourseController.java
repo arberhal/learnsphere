47ae0a33a81b6ad7b7550ddafc2ee1765111ef2c
arberhal <arberhalitix@gmail.com>
2025-12-14 20:01:42 +0100

    Update course endpoint. Closes #6

--- src/main/java/ch/zhaw/learnsphere/controller/CourseController.java
+++ src/main/java/ch/zhaw/learnsphere/controller/CourseController.java
@@ -7,13 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.zhaw.learnsphere.model.Course;
 import ch.zhaw.learnsphere.model.CourseCreateDTO;
+import ch.zhaw.learnsphere.model.CourseUpdateDTO;
 import ch.zhaw.learnsphere.repository.CourseRepository;
+
 
 @RestController
 @RequestMapping("/api/teacher")
@@ -49,6 +52,20 @@ public class CourseController {
     public ResponseEntity<Course> getCourseById(@PathVariable String courseId) {
         return courseRepository.findById(courseId)
                 .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/courses/{courseId}")
+    public ResponseEntity<Course> updateCourse(
+            @PathVariable String courseId,
+            @RequestBody CourseUpdateDTO dto) {
+
+        return courseRepository.findById(courseId)
+                .map(course -> {
+                    course.setTitle(dto.getTitle());
+                    course.setDescription(dto.getDescription());
+                    return ResponseEntity.ok(courseRepository.save(course));
+                })
                 .orElse(ResponseEntity.notFound().build());
     }
 
