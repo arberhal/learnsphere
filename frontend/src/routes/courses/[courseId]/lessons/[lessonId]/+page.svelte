139af06e345748437fa4b32fdca74af4dde6d1e8
arberhal <arberhalitix@gmail.com>
2025-12-16 14:13:27 +0100

    Add lesson page with basic content display. Closes #23

---
+++ frontend/src/routes/courses/[courseId]/lessons/[lessonId]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { data } = $props();
+</script>
+
+<h1>{data.lesson.title}</h1>
+
+<p>{data.lesson.content}</p>
+
+<p>
+    <a href={`/courses/${data.courseId}`}>
+        ← Back to course
+    </a>
+</p>
