c20354626a17e791f30979ffb97666f931c3a931
arberhal <arberhalitix@gmail.com>
2025-12-16 13:33:49 +0100

    Add basic navigation layout using SvelteKit runes. Closes #21

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -8,4 +8,15 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/">Courses</a></li>
+		<li><a href="#">Login</a></li>
+		<li><a href="#">Register</a></li>
+	</ul>
+</nav>
+
+<hr />
+
 {@render children()}
